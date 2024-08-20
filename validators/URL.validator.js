526210e74a33f071a994a88a947d59d04af20e58
Kruddpippin <146596435+Kruddpippin@users.noreply.github.com>
2024-08-20 17:31:17 +0100

    Update URL.validator.js

--- validators/URL.validator.js
+++ validators/URL.validator.js
@@ -1,32 +1,66 @@
 const Joi = require('joi');
 
-const URLSchema = Joi.object({
+const AddURLSchema = Joi.object({
     original: Joi.string()
         .required()
-        .min(10)
+        .min(5) 
         .max(5000)
         .trim(),
-    
-    date: Joi.date()
-        .default(Date.now),
-
+    customID: Joi.string()
+        .optional()
+        .min(2)
+        .max(30)
+        .trim(),
+        short: Joi.string() 
+        .optional() // Should be optional because it is generated server-side
+        .min(4)
+        .max(30)
+        .trim(),
     createdAt: Joi.date()
         .default(Date.now),
 });
 
-async function URLValidationMW (req, res, next) {
-    const URLPayload = req.body
+const UpdateURLSchema = Joi.object({
+    original: Joi.string()
+        .optional() 
+        .min(5)
+        .max(5000)
+        .trim(),
+    short: Joi.string() // Allow updating the short URL
+        .optional()
+        .min(4)
+        .max(30)
+        .trim(),
+    updatedAt: Joi.date()
+        .default(Date.now),
+});
+
+async function AddURLValidationMW (req, res, next) {
+    const URLPayload = req.body;
     
     try {
-        await URLSchema.validateAsync(URLPayload)
-        next()
-
+        await AddURLSchema.validateAsync(URLPayload);
+        next();
     } catch (error) {
         next({
             message: error.details[0].message,
             status: 400
-        })
+        });
     }
 }
 
-module.exports = URLValidationMW;
+async function UpdateURLValidationMW (req, res, next) {
+    const URLPayload = req.body;
+    
+    try {
+        await UpdateURLSchema.validateAsync(URLPayload);
+        next();
+    } catch (error) {
+        next({
+            message: error.details[0].message,
+            status: 400
+        });
+    }
+}
+
+module.exports = { AddURLValidationMW, UpdateURLValidationMW };
