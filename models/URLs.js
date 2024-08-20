3ea93fc875ae816805f5919a02bd7f0e50855d13
Kruddpippin <precious.op2013@gmail.com>
2024-08-20 17:17:05 +0100

    "Added 'short' field to URLSchema in URLs.js"

--- models/URLs.js
+++ models/URLs.js
@@ -6,6 +6,7 @@ const Schema = mongoose.Schema;
 //define URL schema
 const URLSchema = new Schema({
 	original: String,
+	short: String,
 	date: { type: Date, default: Date.now },
 	createdAt: {
 		type: Date,
