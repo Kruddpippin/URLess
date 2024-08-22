e0da88f5aeddacfaeb0c0a169d9d7769f1d93e73
Kruddpippin <precious.op2013@gmail.com>
2024-08-22 14:15:23 +0100

    "Rename 'short' field to 'shortUrl' in URLSchema"

--- models/URLs.js
+++ models/URLs.js
@@ -6,7 +6,7 @@ const Schema = mongoose.Schema;
 //define URL schema
 const URLSchema = new Schema({
 	original: String,
-	short: String,
+	shortUrl: String,
 	date: { type: Date, default: Date.now },
 	createdAt: {
 		type: Date,
